16658e3fb31e2982c4c5cb4f62087fddeaba3513
ralph <oloresralph@gmail.com>
2025-03-20 04:01:53 +1300

    Changed url to cloud backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://cloud-app-b4haceamhabcb2gt.canadacentral-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
